c10f4541619ccb88f155444b2c2dcc24e99ab05b
Gruntov_Dmitrii <gruntoff.dmitry@yandex.ru>
2023-06-19 14:47:41 +0300

    Resolve conflict

--- Project/crypto.py
+++ Project/crypto.py
@@ -1,4 +1,27 @@
+from enums import Crypto
+
+
 class CryptoCurrency:
-    def __init__(self, type="BTC", convertionType="USDT"):
-        self.type = type
-        self.convertionType=convertionType
+    def __init__(self, name, convertionType="USDT"):
+        self.name = name
+        self.type = name
+        self.convertionType = convertionType
+    pass
+
+
+def change_coin_type(crypto_type):
+    global coin
+    if crypto_type == Crypto.BTC:
+        coin = CryptoCurrency('BTC')
+    if crypto_type == Crypto.DOGE:
+        coin = CryptoCurrency('DOGE')
+    if crypto_type == Crypto.ETH:
+        coin = CryptoCurrency('ETH')
+    if crypto_type == Crypto.USDT:
+        coin = CryptoCurrency('USDT')
+    pass
+
+
+coin = CryptoCurrency('USDT')
+
+
