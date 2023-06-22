6c927895e6de87106b0a0c93c69b4e268d59fceb
GruntovDima <98552759+GruntovDima@users.noreply.github.com>
2023-06-22 11:23:29 +0300

    Update crypto_proc.py

--- Project/CRPC/crypto_proc.py
+++ Project/CRPC/crypto_proc.py
@@ -9,10 +9,10 @@ def parse_data_for_graphic(rdata):
 
 def parse_data_for_table(rdata): # Cost Changes request
     answer = object
-    answer.type = "graphic" 
+    answer.type = "table" 
     import json
     data = json.loads(rdata)
-    answer.price = [[i, float(data[i][2])] for i in range(0, len(data))]
-    answer.max = max(answer.price)
+    answer.change = data[1];
+    answer.symbol = data[0];
     return answer
 
